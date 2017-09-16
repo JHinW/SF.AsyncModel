b9d9c1aeb4709455c8d729e916603c660b511475
Jin Wang (Inspur) <v-jiwag@microsoft.com>
2017-09-16 19:37:17 +0800

    initial

---
+++ SF.Async/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SF.Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
