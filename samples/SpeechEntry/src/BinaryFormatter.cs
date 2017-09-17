426018925f1afc37e033780b0934d2913da158a0
Jin Wang (Inspur) <v-jiwag@microsoft.com>
2017-09-17 13:41:43 +0800

    sample build

---
+++ samples/SpeechEntry/src/BinaryFormatter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using Microsoft.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SpeechEntry.src
+{
+    public class BinaryFormatter: InputFormatter
+    {
+        private static Type _supportedType = typeof(byte[]);
+
+        public BinaryFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/octet-stream"));
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/wav"));
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("audio/wav"));
+
+
+        }
+
+        protected override bool CanReadType(Type type)
+        {
+            return type == _supportedType;
+        }
+
+
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var request = context.HttpContext.Request;
+
+            using (var reader = new StreamReader(request.Body))
+            {
+                try
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        request.Body.CopyTo(memoryStream);
+                        memoryStream.Position = 0;
+                        return await InputFormatterResult.SuccessAsync(memoryStream.ToArray());
+                    }
+                }
+                catch
+                {
+                    return await InputFormatterResult.FailureAsync();
+                }
+            }
+
+        }
+
+    }
+}
